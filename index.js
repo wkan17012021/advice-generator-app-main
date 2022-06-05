7a8ae8c68fd0cde3bb7ffca723c3e434a427273d
William <wkan@hotmail.co.uk>
2022-06-05 13:14:24 +0100

    first draft of random quote gen API call.

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-// Info to reach API
+// Info to reach API, DOM selectors
 const adviceSlipUrl = "https://api.adviceslip.com/advice";
 const adviceNumber = document.getElementById("advice-num");
 const randQuote = document.querySelector(".quote-wrapper");
 const quoteBtn = document.getElementById("quote-generator");
+// event listener to fire API fetch
 const handleClick = quoteBtn.addEventListener("click", () => {
   //   console.log("click");
   getRandQuote();
 });
 
+// set empty strings for the inner text properties to auto-populate when the data is obtained
+
 const getRandQuote = async () => {
+  regenId = "";
+  regenQuote = "";
+  adviceNumber.innerText = regenId;
+  randQuote.innerText = regenQuote;
   try {
     const response = await fetch(adviceSlipUrl, { cache: "no-cache" });
     if (response.ok) {
@@ -19,10 +26,10 @@ const getRandQuote = async () => {
       // console.log(quoteId);
       let quote = jsonResponse.slip.advice;
       //   console.log(quote);
+      adviceNumber.innerText += `${quoteId}`;
+      randQuote.innerText += `${quote}`;
     }
   } catch (error) {
     alert(error);
   }
 };
-
-
